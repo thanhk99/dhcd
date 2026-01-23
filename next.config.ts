9ed0c06150f49f12f33903fcd7647220e182abb8
thanhk99 <nhuthanh040804@gmail.com>
2026-01-23 12:11:44 +0700

    refactor: Loại bỏ cấu hình devIndicators thử nghiệm khỏi next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  output: 'standalone',
-  experimental: {
-    // @ts-ignore
-    devIndicators: {
-      appIsrStatus: false,
-      buildActivity: false,
-    },
-  },
+  output: 'standalone'
 };
 
 export default nextConfig;
